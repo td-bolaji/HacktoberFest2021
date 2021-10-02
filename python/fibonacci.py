ecc34a857b7b72078b01c01f2447faf75ea3de72
td-bolaji <60175519+td-bolaji@users.noreply.github.com>
2021-10-02 17:47:54 +0100

    Update fibonacci.py
    Changed a little code to make it better

--- python/fibonacci.py
+++ python/fibonacci.py
@@ -1,7 +1,7 @@
+
 def fibonacci(n):
     """
     Return n-th Fibonacci number.
-
     Raises TypeError if n is not integer.
     Raises ValueError if n is negative.
     :param n: (int) natural number
@@ -17,3 +17,18 @@ def fibonacci(n):
             c = a + b
             a, b = b, c
         return b
+
+
+def run():
+    num = int(input('Enter the n-th Fibonacci number you want to get: '))
+    check(num)
+    print(fibonacci(num))
+
+
+def check(t):
+    if type(t) != int:
+        print('Please enter a NUMBER')
+        run()
+
+
+run()
