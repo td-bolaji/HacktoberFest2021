5f6de89ab6683926417eb396c1a02e96b790f9a4
td-bolaji <60175519+td-bolaji@users.noreply.github.com>
2021-10-02 17:54:31 +0100

    Update figure.py
    Made code display time

--- python/figure.py
+++ python/figure.py
@@ -1,8 +1,16 @@
 import turtle
+from turtle import Screen
+from datetime import datetime
+
+now = datetime.now()
+dateTime = now.strftime("%d/%m/%Y %H:%M:%S")
+
+screen = Screen()
+TURTLE_SIZE = 20
 
 t = turtle.Turtle()
 s = turtle.Screen()
-s.bgcolor("black")
+s.bgcolor("white")
 t.pencolor("green")
 t.speed(0.2)
 t.penup()
@@ -10,13 +18,16 @@ t.goto(0, 200)
 t.pendown()
 a = 0
 b = 0
-while True :
+while True:
     t.forward(a)
     t.right(b)
     a += 3
     b += 1
-    if b == 200 :
+    if b == 200:
         break
     t.hideturtle()
 
+turtle.penup()
+turtle.goto(TURTLE_SIZE/2 - screen.window_width()/2, screen.window_height()/2 - (TURTLE_SIZE/2) - 20)
+turtle.write(dateTime, move=False, font=('monaco', 15, 'bold'), align='left')
 turtle.done()
